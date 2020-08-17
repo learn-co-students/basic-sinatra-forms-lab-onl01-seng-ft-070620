b958281a1d47f5c9682a6a9b883fa11266a51117
JacksonReynolds <jwreynod@gmail.com>
2020-08-17 14:48:59 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do
+        erb :newteam
+    end
 
+    post '/team' do
+        @team_info = {}
+        params.each do |k, v|
+            @team_info[k] = v
+        end
+        # binding.pry
+        erb :team
+    end
 
 end
